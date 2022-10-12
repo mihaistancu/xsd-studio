d2eb4b83ac994a233157ced533c210bb81ea443d
mihaistancu <stancu_meehigh@yahoo.com>
2022-10-12 13:02:18 +0200

    Bind search parameters

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -18,6 +19,7 @@ import { CdmReleaseSearchComponent } from './cdm-release-search/cdm-release-sear
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
