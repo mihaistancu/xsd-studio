1092b651eac235f36aacde0f20073e6dd3f401ac
mihaistancu <stancu_meehigh@yahoo.com>
2022-10-12 15:56:22 +0200

    Display edit button

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { AddCdmReleaseComponent } from './add-cdm-release/add-cdm-release.component';
 import { AddChangeRequestComponent } from './add-change-request/add-change-request.component';
 import { CdmReleaseSearchComponent } from './cdm-release-search/cdm-release-search.component';
+import { EditCdmReleaseComponent } from './edit-cdm-release/edit-cdm-release.component';
 
 const routes: Routes = [
   {
     path: 'new-cdm-release',
     component: AddCdmReleaseComponent,
+  },
+  {
+    path: 'edit-cdm-release/:cdmCode',
+    component: EditCdmReleaseComponent,
   },
   {
     path: 'cdm-release-search',
